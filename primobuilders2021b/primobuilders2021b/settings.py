1f153314499ed43cec3f9cdffb9ed7f4b22f3da3
bcostantino <costan96@students.rowan.edu>
2021-07-06 19:49:54 -0400

    ss

--- primobuilders2021b/primobuilders2021b/settings.py
+++ primobuilders2021b/primobuilders2021b/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from secret_settings import *
+from . import secret_settings as ss
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = _SECRET_KEY
+SECRET_KEY = ss.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
