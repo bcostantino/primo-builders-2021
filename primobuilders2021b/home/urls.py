030c311cdbdf66bc35cb01f0014bdca0beca5ad2
Samcos38 <72896205+Samcos38@users.noreply.github.com>
2021-08-09 12:42:12 -0400

    getttit

--- primobuilders2021b/home/urls.py
+++ primobuilders2021b/home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Home'),
+    path('about/', views.about, name='About'),
+    path('services/', views.services, name='Services'),
+    path('areasOfServ/', views.areasOfServ, name='areasOfServ'),
+    path('consultation', views.consultation, name='consultation'),
+
 ]
--- primobuilders2021b/home/views.py
+++ primobuilders2021b/home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def services(request):
+    return render(request, 'home/services.html')
+
+def areasOfServ(request):
+    return render(request, 'home/areasOfServ.html')
+
+def consultation(request):
+    return render(request, 'home/consultation')
